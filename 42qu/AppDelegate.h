b50325d7630c278f4f82b9489abfebf674330c7c
Alex Rezit <AlexRezit@seymourdev.com>
2012-05-22 13:53:02 +0800

    Initial Commit

---
+++ 42qu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  42qu
+//
+//  Created by Alex Rezit on 12-5-22.
+//  Copyright (c) 2012年 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 42qu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  42qu
+//
+//  Created by Alex Rezit on 12-5-22.
+//  Copyright (c) 2012年 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
