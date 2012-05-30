dd649a6dd51e8641df2b18cd0dd699a4db898c95
AlexRezit <AlexRezit@gmail.com>
2012-05-30 14:55:46 +0800

    Add register view.

---
+++ 42qu/RegisterView.h
@@ -0,0 +1,13 @@
+//
+//  RegisterView.h
+//  42qu
+//
+//  Created by Alex Rezit on 12-5-30.
+//  Copyright (c) 2012年 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterView : UIView
+
+@end
---
+++ 42qu/RegisterView.m
@@ -0,0 +1,31 @@
+//
+//  RegisterView.m
+//  42qu
+//
+//  Created by Alex Rezit on 12-5-30.
+//  Copyright (c) 2012年 Seymour Dev. All rights reserved.
+//
+
+#import "RegisterView.h"
+
+@implementation RegisterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
