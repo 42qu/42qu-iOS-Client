94522ba908f10ebf82f2386d6599db02baba7320
AlexRezit <AlexRezit@gmail.com>
2012-06-25 16:11:15 +0800

    Add 3 child controllers of update root vc.

---
+++ 42qu/UpdateRootRecommendViewController.h
@@ -0,0 +1,13 @@
+//
+//  UpdateRootRecommendViewController.h
+//  42qu
+//
+//  Created by Alex Rezit on 12-6-25.
+//  Copyright (c) 2012年 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpdateRootRecommendViewController : UITableViewController
+
+@end
