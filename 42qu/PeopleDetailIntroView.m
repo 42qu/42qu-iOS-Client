a188c61b61f9edc614271aef9ff59fdd62b23340
AlexRezit <AlexRezit@gmail.com>
2012-06-26 23:33:08 +0800

    Oh yeah I am fixing something stupid.

--- 42qu/PeopleDetailIntroView.m
+++ 42qu/PeopleDetailIntroView.m
@@ -10,4 +10,22 @@
 
 @implementation PeopleDetailIntroView
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
+
 @end
